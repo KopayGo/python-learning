94605883a84419809480a500fc970278a353e86c
KopayGo <vinsent_1@mail.ru>
2025-10-31 16:09:04 +0300

    Day 1

---
+++ day1.py
@@ -0,0 +1,14 @@
+your_name = "KopayGo"
+your_city = "Moscow"
+years_of_experience = 0
+target_salary = 250000
+current_focus = "Python"
+print("=" * 60)
+print("📋 Profile".center(60))
+print("" * 60)
+print(f"Name: {your_name}")
+print(f"City: {your_city}")
+print(f"Experience: {years_of_experience}")
+print(f"Target salary: {target_salary}")
+print(f"Current focus: {current_focus}")
+print("=" * 60)
