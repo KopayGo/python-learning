6f763593de43721601bc2719b836be01696a575b
KopayGo <vinsent_1@mail.ru>
2025-11-01 11:40:17 +0300

    day1.py

--- day1.py
+++ day1.py
@@ -3,6 +3,8 @@ your_city = "Moscow"
 years_of_experience = 0
 target_salary = 250000
 current_focus = "Python"
+start_date_learning = "31.10.2025"
+hours_per_day = "2"
 print("=" * 60)
 print("📋 Profile".center(60))
 print("" * 60)
@@ -11,4 +13,6 @@ print(f"City: {your_city}")
 print(f"Experience: {years_of_experience}")
 print(f"Target salary: {target_salary}")
 print(f"Current focus: {current_focus}")
+print(f"Learning start day: {start_date_learning}")
+print(f"Hours per day: {hours_per_day}")
 print("=" * 60)
